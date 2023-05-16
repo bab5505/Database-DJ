232e2e99b1a77b2195815b57500d93928d44d50d
bab5505 <122764274+bab5505@users.noreply.github.com>
2023-05-16 12:07:32 -0700

    Update forms.py

--- forms.py
+++ forms.py
@@ -4,21 +4,20 @@ from wtforms.validators import DataRequired
 
 
 class PlaylistForm(FlaskForm):
-    """Form for adding playlists."""
+    """Adding playlists."""
 
     name = StringField('Name', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired()])
 
 
 class SongForm(FlaskForm):
-    """Form for adding songs."""
+    """Adding songs."""
 
     title = StringField('Title', validators=[DataRequired()])
     artist = StringField('Artist', validators=[DataRequired()])
 
 
-# DO NOT MODIFY THIS FORM - EVERYTHING YOU NEED IS HERE
 class NewSongForPlaylistForm(FlaskForm):
-    """Form for adding a song to playlist."""
+    """Adding a song to playlist."""
 
     song = SelectField('Song To Add', coerce=int)
